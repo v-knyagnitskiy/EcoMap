aea4bf1d832ef5d4f9663ad95f363b3ba4110ee4
v-knyagnitskiy <knyagnitskiy.vitaly@gmail.com>
2016-08-03 15:55:17 +0000

    Update ecomap_config_builder.sh

--- ecomap/bin/ecomap_config_builder.sh
+++ ecomap/bin/ecomap_config_builder.sh
@@ -1,7 +1,7 @@
 # !/usr/bin/env bash
 # Script run config builder 
 
-PRODROOT=~/home/workspace/test_ecomap/ecomap
+PRODROOT=~/home/travis/build/v-knyagnitskiy/EcoMap/ecomap
 PYSRCROOT=${PRODROOT}/src/python
 CONFROOT=${PRODROOT}/etc
 PYTHONPATH=$PYSRCROOT
