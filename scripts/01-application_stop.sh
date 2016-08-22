ec8022c180487ce9be49f4371c5ce4ea0aac7d3d
v-knyagnitskiy <knyagnitskiy.vitaly@gmail.com>
2016-08-22 00:44:51 +0000

    Update 01-application_stop.sh

--- scripts/01-application_stop.sh
+++ scripts/01-application_stop.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-state=`pgrep apache2`
+state_apache=`pgrep apache2`
+state_app=`ls /etc/apache2/sites-enabled | grep ecomap.conf`
 
-if [[ -n  $state ]]
+if [[ -n  $state_apache ]]
   then
     service apache2 stop
+fi
+
+if [[ -n  $state_app ]]
+  then
+    a2dissite ecomap
 fi
