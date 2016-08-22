59c03929d99fd260fc14917a5497ab86c0db0fc9
v-knyagnitskiy <knyagnitskiy.vitaly@gmail.com>
2016-08-22 07:46:02 +0000

    Update 02-after_install.sh

--- scripts/02-after_install.sh
+++ scripts/02-after_install.sh
@@ -2,5 +2,7 @@
 
 cp /var/www/ecomap/etc/_ecomap.apache.conf /etc/apache2/sites-available/ecomap.conf
 
+chmod 644 /etc/apache2/sites-available/ecomap.conf
+
 sed -i -e "s|ServerName xxxxx|ServerName $(cat /root/app_data/EC2InstanceElasticIp)|g" /etc/apache2/sites-available/ecomap.conf
 sed -i -e "s|ServerAlias xxxxx|ServerAlias www.$(cat /root/app_data/EC2InstanceElasticIp)|g" /etc/apache2/sites-available/ecomap.conf
