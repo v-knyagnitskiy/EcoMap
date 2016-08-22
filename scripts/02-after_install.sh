318a0495735cc9852e22e318c2ff47d60bb73ced
v-knyagnitskiy <knyagnitskiy.vitaly@gmail.com>
2016-08-22 01:48:23 +0000

    Create 02-after_install.sh

---
+++ scripts/02-after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp /var/www/ecomap/etc/_ecomap.apache.conf /etc/apache2/sites-available/ecomap.conf
+
+sed -i -e "s|ServerName xxxxx|ServerName $(cat /root/app_data/EC2InstanceElasticIp)|g" /etc/apache2/sites-available/ecomap.conf
+sed -i -e "s|ServerAlias xxxxx|ServerAlias www.$(cat /root/app_data/EC2InstanceElasticIp)|g" /etc/apache2/sites-available/ecomap.conf
